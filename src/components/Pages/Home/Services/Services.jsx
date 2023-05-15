95674852f95694f6e07bcb5bf744a451daf30c64
jubair-jx <jubair.official97@gmail.com>
2023-05-15 12:15:14 +0600

    Sevice Part Done

---
+++ src/components/Pages/Home/Services/Services.jsx
@@ -0,0 +1,32 @@
+import { data } from "autoprefixer";
+import React, { useEffect, useState } from "react";
+import ShowService from "./ShowService";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("service.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center mt-8">
+        <p className="text-xl font-bold text-orange-400 mt-5">Service</p>
+        <h1 className="text-4xl font-bold mt-5">Our Service Area</h1>
+        <p className="text-sm mt-5">
+          the majority have suffered alteration in some form, <br /> by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+      </div>
+      <div className="grid md:grid-cols-3 mx-auto gap-10 mt-5 mb-12">
+        {services.map((service) => (
+          <ShowService key={service._id} service={service}></ShowService>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/components/Pages/Home/Services/ShowService.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const ShowService = ({ service }) => {
+  const { price, img, title } = service;
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img className="h-60 w-full" src={img} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p className="text-orange-500 text-lg font-semibold">
+            Price : {price}
+          </p>
+          <div className="card-actions justify-end">
+            <button className="text-orange-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowService;
