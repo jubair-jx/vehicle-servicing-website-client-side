04df4e235dfd4d62aa1297b3b463327c7111c6b0
jubair-jx <jubair.official97@gmail.com>
2023-05-16 14:20:45 +0600

    Delete Functionlity DOne

--- src/components/BookingServices/BookingServices.jsx
+++ src/components/BookingServices/BookingServices.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Providers/Provider";
+import BookingServicesRow from "./BookingServicesRow";
 
 const BookingServices = () => {
   const { users } = useContext(AuthContext);
@@ -10,9 +11,54 @@ const BookingServices = () => {
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
+
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure want to delete?");
+    if (proceed) {
+      fetch(`http://localhost:5000/booking/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("SuccessFully Deleted");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h2>Book Item : {bookings.length} </h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingServicesRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+                // handleBookingConfirm={handleBookingConfirm}
+              ></BookingServicesRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
